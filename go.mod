c915f48d0d64fd35f12d475988eb194da7249e2b
Mustom <liucheng@tuya.com>
2022-02-19 16:27:12 +0800

    [feature] 增加 grpc 示例 +

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module gocase
 go 1.15
 
 require (
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/marusama/cyclicbarrier v1.1.0
 	github.com/petermattis/goid v0.0.0-20220111183729-e033e1e0bdb5
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+	google.golang.org/grpc v1.44.0
+	google.golang.org/protobuf v1.27.1
 )
---
+++ rpc/grpc/client/main.go
@@ -0,0 +1,42 @@
+// Package main implements a client for Greeter service.
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	pb "gocase/rpc/grpc/protos"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+}
