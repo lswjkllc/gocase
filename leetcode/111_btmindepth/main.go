91108f4368da284a22cda009e1908480688b5c65
Mustom <liucheng@tuya.com>
2022-03-14 21:31:16 +0800

    [feature] 增加 leetcode 111 btmindepth with bfs/dfs

--- leetcode/111_btmindepth/main.go
+++ leetcode/111_btmindepth/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 /**
  * Definition for a binary tree node.
@@ -17,7 +20,7 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func minDepth(root *TreeNode) int {
+func minDepthWithBfs(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
@@ -46,11 +49,37 @@ func minDepth(root *TreeNode) int {
 	return depth
 }
 
+func minDepthWithDfs(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+	minD := math.MaxInt32
+	if root.Left != nil {
+		minD = min(minDepthWithDfs(root.Left), minD)
+	}
+	if root.Right != nil {
+		minD = min(minDepthWithDfs(root.Right), minD)
+	}
+	return minD + 1
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
 func main() {
 	// [1,2,3,4,null,null,5]
 	root1 := &TreeNode{Val: 1, Left: &TreeNode{Val: 2, Left: &TreeNode{Val: 4}}, Right: &TreeNode{Val: 3, Right: &TreeNode{Val: 5}}}
-	fmt.Println(minDepth(root1))
+	bfsD1, dfsD1 := minDepthWithBfs(root1), minDepthWithDfs(root1)
+	fmt.Println(bfsD1 == dfsD1, bfsD1, dfsD1)
 	// [3,9,20,null,null,15,7]
 	root2 := &TreeNode{Val: 3, Left: &TreeNode{Val: 9}, Right: &TreeNode{Val: 20, Left: &TreeNode{Val: 15}, Right: &TreeNode{Val: 7}}}
-	fmt.Println(minDepth(root2))
+	bfsD2, dfsD2 := minDepthWithBfs(root2), minDepthWithDfs(root2)
+	fmt.Println(bfsD2 == dfsD2, bfsD2, dfsD2)
 }
