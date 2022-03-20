f66f1418ea93e2a07cd273bcad7a13f560c8bf19
Mustom <liucheng@tuya.com>
2022-03-20 14:29:29 +0800

    [feature] 增加 leetcode 121 122 max profit +

--- leetcode/121_maxprofit/main.go
+++ leetcode/121_maxprofit/main.go
@@ -1,29 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 /*
 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
 
 ===> 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。
+===> k = 1 表示只能进行 1 次交易
 
 设计一个算法来计算你所能获取的最大利润。
 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
 */
 // https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
-
-func maxProfit(prices []int) int {
-	res, pLen := 0, len(prices)
-	if pLen == 0 {
-		return res
-	}
-	curMin := prices[0]
-	for sell := 1; sell < pLen; sell++ {
-		curMin = min(curMin, prices[sell])
-		res = max(res, prices[sell]-curMin)
-	}
-	return res
-}
 
 func min(a, b int) int {
 	if a > b {
@@ -39,7 +30,73 @@ func max(a, b int) int {
 	return a
 }
 
+func MaxProfit1(prices []int) int {
+	res, pLen := 0, len(prices)
+	if pLen == 0 {
+		return res
+	}
+	curMin := prices[0]
+	for sell := 1; sell < pLen; sell++ {
+		curMin = min(curMin, prices[sell])
+		res = max(res, prices[sell]-curMin)
+	}
+	return res
+}
+
+func MaxProfit2(prices []int) int {
+	pLen := len(prices)
+	if pLen <= 1 {
+		return 0
+	}
+	// dp[n][k][2]
+	// k = 1 => dp[n][2]
+	// k - 1 = 0 => dp[i-1][0]
+	/*
+		k = 0 的 base case，所以 dp[i-1][0][0] = 0。
+		现在发现 k 都是 1，不会改变，即 k 对状态转移已经没有影响了。
+		可以进行进一步化简去掉所有 k
+	*/
+	dp := make([][2]int, pLen)
+	for i := 0; i < pLen; i++ {
+		if i-1 < 0 {
+			// 根据状态转移方程可得：
+			//   dp[i][0]
+			// = max(dp[-1][0], dp[-1][1] + prices[i])
+			// = max(0, -infinity + prices[i]) = 0
+			dp[i][0] = 0
+			// 根据状态转移方程可得：
+			//   dp[i][1]
+			// = max(dp[-1][1], dp[-1][0] - prices[i])
+			// = max(-infinity, 0 - prices[i])
+			// = -prices[i]
+			dp[i][1] = -prices[i]
+			continue
+		}
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+		dp[i][1] = max(dp[i-1][1], -prices[i])
+	}
+
+	return dp[pLen-1][0]
+}
+
+/*
+注意一下状态转移方程，新状态只和相邻的一个状态有关。
+其实不用整个dp数组，只需要一个变量储存相邻的那个状态就足够了，这样可以把空间复杂度降到 O(1)
+*/
+func MaxProfit(prices []int) int {
+	pLen := len(prices)
+	if pLen < 1 {
+		return 0
+	}
+	dp_i_0, dp_i_1 := 0, math.MinInt32
+	for i := 0; i < pLen; i++ {
+		dp_i_0 = max(dp_i_0, dp_i_1+prices[i])
+		dp_i_1 = max(dp_i_1, -prices[i])
+	}
+	return dp_i_0
+}
+
 func main() {
-	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
-	fmt.Println(maxProfit([]int{7, 6, 4, 3, 1}))
+	fmt.Println("prices: [7, 1, 5, 3, 6, 4] k: 1 => 5", MaxProfit([]int{7, 1, 5, 3, 6, 4}) == 5)
+	fmt.Println("prices: [7, 6, 4, 3, 1] k: 1 => 0", MaxProfit([]int{7, 6, 4, 3, 1}) == 0)
 }
--- leetcode/122_maxprofit/main.go
+++ leetcode/122_maxprofit/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 /*
 给定一个数组 prices ，其中 prices[i] 表示股票第 i 天的价格。
@@ -8,13 +11,14 @@ import "fmt"
 ===> 在每一天，你可能会决定购买和/或出售股票。
 ===> 你在任何时候 最多 只能持有 一股 股票。
 ===> 你也可以购买它，然后在 同一天 出售。
+===> k = +infinity 表示不限制交易次数
 
 返回 你能获得的 最大 利润 。
 */
 // https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
 
 // 该方法在 prices 很大的时候，会超过 leetcode 给的时间限制
-func MaxProfit(prices []int) int {
+func MaxProfit1(prices []int) int {
 	pLen := len(prices)
 	memo := map[int]int{}
 	for i := 0; i < pLen; i++ {
@@ -55,7 +59,7 @@ func Max(a, b int) int {
 }
 
 // 最优解：贪心算法
-func maxProfit(prices []int) int {
+func MaxProfit2(prices []int) int {
 	maxprofit := 0
 	for i := 1; i < len(prices); i++ {
 		if prices[i] > prices[i-1] {
@@ -65,9 +69,50 @@ func maxProfit(prices []int) int {
 	return maxprofit
 }
 
+func MaxProfit3(prices []int) int {
+	pLen := len(prices)
+	if pLen < 1 {
+		return 0
+	}
+	/*
+			如果k为正无穷，那么就可以认为k和k - 1是一样的。框架：
+				dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i])
+				dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i])
+		            		= max(dp[i-1][k][1], dp[i-1][k][0] - prices[i])
+			我们发现数组中的 k 已经不会改变了，也就是说不需要记录 k 这个状态了：
+				dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+				dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+
+	*/
+	dp := make([][2]int, pLen)
+	for i := 0; i < pLen; i++ {
+		if i-1 < 0 {
+			dp[i][0] = 0
+			dp[i][1] = -prices[i]
+			continue
+		}
+		dp[i][0] = Max(dp[i-1][0], dp[i-1][1]+prices[i])
+		dp[i][1] = Max(dp[i-1][1], dp[i-1][0]-prices[i])
+	}
+	return dp[pLen-1][0]
+}
+
+func MaxProfit(prices []int) int {
+	pLen := len(prices)
+	if pLen < 1 {
+		return 0
+	}
+	dp_i_0, dp_i_1, temp := 0, math.MinInt32, 0
+	for i := 0; i < pLen; i++ {
+		temp = dp_i_0
+		dp_i_0 = Max(dp_i_0, dp_i_1+prices[i])
+		dp_i_1 = Max(dp_i_1, temp-prices[i])
+	}
+	return dp_i_0
+}
+
 func main() {
-	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
-	fmt.Println(maxProfit([]int{1, 2, 3, 4, 5}))
-	fmt.Println(maxProfit([]int{7, 6, 4, 3, 1}))
-	fmt.Println(maxProfit([]int{1, 2, 3, 4, 5}))
+	fmt.Println("prices: [7, 1, 5, 3, 6, 4] k: +infinity => 7", MaxProfit3([]int{7, 1, 5, 3, 6, 4}) == 7)
+	fmt.Println("prices: [1, 2, 3, 4, 5] k: +infinity => 4", MaxProfit3([]int{1, 2, 3, 4, 5}) == 4)
+	fmt.Println("prices: [7, 6, 4, 3, 1] k: +infinity => 0", MaxProfit3([]int{7, 6, 4, 3, 1}) == 0)
 }
